99df5c80849ecf19b2d325befef09f940605b6ae
k.wilaszek <k.wilaszek@ocado.com>
2017-09-01 01:13:06 +0200

    Films resource added - add film to database test

---
+++ src/test/groovy/krzysztofk/video/rental/dao/FilmDAOTest.groovy
@@ -0,0 +1,38 @@
+package krzysztofk.video.rental.dao
+
+import io.dropwizard.testing.junit.DAOTestRule
+import krzysztofk.video.rental.api.Film
+import krzysztofk.video.rental.api.FilmType
+import org.junit.Rule
+import spock.lang.Specification
+
+import java.util.concurrent.Callable
+
+class FilmDAOTest extends Specification {
+
+    @Rule
+    DAOTestRule database = DAOTestRule.newBuilder().addEntityClass(Film).build()
+    def filmDAO = new FilmDAO(database.sessionFactory)
+
+    def "should add film"() {
+        given:
+        def filmToAdd = new Film(title: "Some film", type: FilmType.OLD_FILM)
+
+        when:
+        def addedFilm = database.inTransaction addFilm(filmToAdd)
+
+        then:
+        addedFilm.id != null
+        addedFilm.title == "Some film"
+        addedFilm.type == FilmType.OLD_FILM
+    }
+
+    def addFilm(Film film) {
+        new Callable<Film>() {
+            @Override
+            Film call() throws Exception {
+                return filmDAO.add(film)
+            }
+        }
+    }
+}
