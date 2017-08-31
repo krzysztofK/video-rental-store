20aaaf1b8469df397f9a14c1b351fe71e0ab1e54
k.wilaszek <k.wilaszek@ocado.com>
2017-08-31 22:35:49 +0200

    Films resource added - status code changed

--- src/main/java/krzysztofk/video/rental/resources/FilmResource.java
+++ src/main/java/krzysztofk/video/rental/resources/FilmResource.java
@@ -7,13 +7,14 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/films")
 @Produces(MediaType.APPLICATION_JSON)
 public class FilmResource {
 
   @POST
-  public Film saveFilm(@Valid Film film) {
-    return film;
+  public Response saveFilm(@Valid Film film) {
+    return Response.status(Response.Status.CREATED).entity(film).build();
   }
 }
