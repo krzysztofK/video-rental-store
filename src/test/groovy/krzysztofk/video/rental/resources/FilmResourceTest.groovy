80558706121b17fb8df2731673fd596820f0251c
k.wilaszek <k.wilaszek@ocado.com>
2017-09-01 23:13:15 +0200

    Films resource added - film resource tests

---
+++ src/test/groovy/krzysztofk/video/rental/resources/FilmResourceTest.groovy
@@ -0,0 +1,57 @@
+package krzysztofk.video.rental.resources
+
+import io.dropwizard.testing.junit.ResourceTestRule
+import krzysztofk.video.rental.api.Film
+import krzysztofk.video.rental.api.FilmType
+import krzysztofk.video.rental.dao.FilmDAO
+import org.junit.ClassRule
+import spock.lang.Shared
+import spock.lang.Specification
+
+import javax.ws.rs.client.Entity
+
+class FilmResourceTest extends Specification {
+
+    @Shared
+    def filmDAO = Mock(FilmDAO)
+
+    @Shared
+    @ClassRule
+    ResourceTestRule resources = ResourceTestRule.builder().addResource(new FilmResource(filmDAO)).build()
+
+    def setupSpec() {
+        filmDAO.add(_) >> { Film film -> new Film(id: 1, title: film.title, type: film.type) }
+        filmDAO.getById(100) >> film100()
+    }
+
+    def "should add film resource"() {
+        given:
+        def filmToAdd = new Film(title: "title", type: FilmType.NEW_RELEASE)
+
+        when:
+        def response = resources.target("/films").request().post(Entity.json(filmToAdd))
+
+        then:
+        response.status == 201
+        def responseFilm = response.readEntity(Film)
+        responseFilm.id == 1
+        responseFilm.title == "title"
+        responseFilm.type == FilmType.NEW_RELEASE
+    }
+
+    def "should find film by id"() {
+        when:
+        def response = resources.target("/films/100").request().get()
+
+        then:
+        response.status == 200
+        def responseFilm = response.readEntity(Film)
+        responseFilm.id == film100().id
+        responseFilm.title == film100().title
+        responseFilm.type == film100().type
+    }
+
+    def film100() {
+        new Film(id: 100, title: "film 100", type: FilmType.REGULAR_FILM)
+    }
+}
